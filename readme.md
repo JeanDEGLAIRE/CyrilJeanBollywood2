77633b3c2bf7974ea83b19aa70f662dcf0cce811
Jean D <jean.deglaire@gmail.com>
2017-08-09 16:52:08 +0200

    Le projet de Cyril et Jean

---
+++ readme.md
@@ -0,0 +1,5 @@
+Projet : Bollywood
+Auteurs : Cyril et Jean
+Version : 0.3
+
+Ce projet permet de choisir son acteur de Bollywood préféré et affiche sa fiche (nom prénom et photo)
---
+++ src/fr/cyriljean/bollywood/bean/BollywoodController.java
@@ -0,0 +1,61 @@
+package fr.cyriljean.bollywood.bean;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean
+@SessionScoped
+public class BollywoodController {
+	
+	private String actor = "";
+	private String name = "";
+	private String img = "";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getActor() {
+		return actor;
+	}
+
+	public void setActor(String actor) {
+		switch (actor) {
+		case "aamir":
+			this.actor = actor;
+			this.name = "Aamir Khan";
+			this.img = "/resources/img/aamir.jpg";
+			break;
+			
+		case "karishma":
+			this.actor = actor;
+			this.name = "Karishma Kapoor";
+			this.img = "/resources/img/karishma.jpg";
+			break;
+
+		default:
+			this.actor = "";
+			this.name = "Tunak Tunak Tun !";
+			this.img = "/resources/img/tunak.gif";
+			break;
+		}
+		this.actor = actor;
+	}
+
+
+
+}
